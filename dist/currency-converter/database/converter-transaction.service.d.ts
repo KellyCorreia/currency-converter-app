a8386a6012eecc103a90d1dda412f091c27c3667
KellyCorreia <ksuenny@gmail.com>
2023-08-20 18:34:03 -0300

    Revert "first commit"
    This reverts commit 64f6657970c28002dd2dd6154271a04db6988361.

--- dist/currency-converter/database/converter-transaction.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Repository } from 'typeorm';
-import { ConverterTransaction } from './converter-transaction.entity';
-export declare class ConverterTransactionService {
-    private ConverterTransactionRepository;
-    constructor(ConverterTransactionRepository: Repository<ConverterTransaction>);
-    createTransaction(userId: string, currencyFrom: string, amountFrom: number, currencyTo: string, conversionIndex: number): Promise<ConverterTransaction>;
-    findAllTransactions(): Promise<ConverterTransaction[]>;
-    getTransactionById(id: number): Promise<ConverterTransaction>;
-}
--- dist/currency-converter/database/database.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { Database } from 'sqlite3';
-export declare class DatabaseService {
-    private db;
-    constructor();
-    getDbInstance(): Database;
-}
